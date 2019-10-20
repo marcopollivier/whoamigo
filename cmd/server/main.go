051f994d499f0a7beda5608d903daeb360d1e906
Marco Paulo Ollivier <marcopollivier@gmail.com>
2019-10-20 00:14:56 -0300

    Simplifying the project

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,39 @@
 package main
 
 import (
-	"github.com/marcopollivier/whoamigo/internal/server/http"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
 func main() {
-	http.Init()
+	Init()
+}
+
+func Init() {
+	http.HandleFunc("/", meHandler)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func now() string {
+	var timeNow = time.Now()
+	return timeNow.Format("2006-01-02T15:04:05.000Z")
+}
+
+func meHandler(responseWriter http.ResponseWriter, req *http.Request) {
+	responseWriter.Header().Set("Content-Type", "application/json")
+
+	var hostname, _ = os.Hostname()
+	var payload = Me{hostname, now()}
+
+	returnBody, _ := json.Marshal(payload)
+
+	_, _ = responseWriter.Write(returnBody)
+}
+
+type Me struct {
+	Me        string `json:"me"`
+	Timestamp string `json:"timestamp"`
 }
