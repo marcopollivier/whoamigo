8d460f2082d581e6775c7bb9c841a67c444eeeca
Marco Paulo Ollivier <marcopollivier@gmail.com>
2019-10-20 00:07:34 -0300

    Create basic REST "Who am I?" structure

---
+++ cmd/server/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/marcopollivier/whoamigo/internal/server/http"
+)
+
+func main() {
+	http.Init()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/marcopollivier/whoamigo
+
+go 1.12
